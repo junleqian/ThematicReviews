959a07218fb5c9707cb675ee8d2c6aacd93dc37b
Junle Qian <calebqian.uiuc@gmail.com>
2018-05-28 22:40:23 -0700

    Add heap implementation framework

---
+++ DataStructureImplementation/Heap/heap.py
@@ -0,0 +1,43 @@
+"""
+Two classes represents the min-heap and max-heap.
+push - O(1)
+remove - O(lgN)
+pop/peek - O(1)
+
+"""
+
+class MinHeap(object):
+    def __init__(self, given_list=[]):
+        pass
+
+    def push(self, item, value):
+        pass
+
+    def pop(self):
+        # pop the one carries minimum value
+        pass
+
+    def remove(self, value):
+        pass
+
+    def peek(self):
+        # return the minimum without popping it
+        pass
+
+class MaxHeap(object);
+    def __init__(self, given_list=[]):
+        pass
+
+    def push(self, item, value):
+        pass
+
+    def pop(self):
+        # pop the maximum
+        pass
+
+    def remove(self, value):
+        pass
+
+    def peek(self):
+        # return the maximum without popping it
+        pass
